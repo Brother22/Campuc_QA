a634e70ba80bad6f36931b8514a048ba70523c79
sun <sun_yyang@qq.com>
2018-12-12 20:25:47 +0800

    添加实名匿名

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -8,16 +8,55 @@ Page({
       title: '详情页面'
     })
   },
+  // submit:function(){
+  //   var that = this
+  //   const db = wx.cloud.database()
+  //   db.collection("answer_brief").add({
+  //     data: {
+  //       title: e.detail.value.textarea1,
+  //       uid: app.globalData.id,
+  //       qid: that.data.art.qid,
+  //       type: 1
+  //     },
+  //     success(res) {
+  //       db.collection("answer_brief").where({ title: e.detail.value.textarea1 }).get({
+  //         success(res) {
+  //           db.collection("answer").add({
+  //             data: {
+  //               content: e.detail.value.textarea2,
+  //               uid: app.globalData.id,
+  //               aid: res.data[0]._id
+  //             },
+  //             success(res) {
+  //               wx.navigateTo({
+  //                 url: '../fsucc/fsucc'
+  //               })
+  //             }
+  //           })
+  //         }
+  //       })
+  //     }
+  //   })
+  // },
+  niming:function(e){
+    var that=this
+    if (e.detail.value == '') {
+      that.data.art.type=0
+    }
+    else {
+      that.data.art.type=1
+    }
+  },
   bindFormSubmit:function(e){
     var that = this
     const db = wx.cloud.database()
-    console.log(that.data.art)
+    console.log(e.detail.value)
     db.collection("answer_brief").add({
       data:{
         title: e.detail.value.textarea1,
         uid: app.globalData.id,
         qid:that.data.art.qid,
-        aid:0
+        type:that.data.art.type
       },
       success(res){
         db.collection("answer_brief").where({ title: e.detail.value.textarea1}).get({
@@ -32,7 +71,13 @@ Page({
           data: {
             content: e.detail.value.textarea2,
             uid: app.globalData.id,
-            aid: res.data[0]._id
+            aid: res.data[0]._id,
+            type:that.data.art.type
+          },
+          success(res){
+            wx.navigateTo({
+              url: '../fsucc/fsucc'
+            })
           }
         })
       }
--- pages/answerdetail/detail.js
+++ pages/answerdetail/detail.js
@@ -22,6 +22,7 @@ fcomment:function(){
   fanswer:function(){
     var that=this
     const db = wx.cloud.database()
+    if(that.data.art.type === 0){
     db.collection('myloveperson').where({ uid: that.data.author.uid}).get({
       success(res){
         if (res.data.length === 0) {
@@ -42,6 +43,7 @@ fcomment:function(){
           }
       }
     })
+    }
     // db.collection('myloveperson').add({
     //   data:that.data.author
     // })
@@ -53,18 +55,49 @@ fcomment:function(){
     // console.log("123"+options.id)
     db.collection('answer').where({ aid: options.id }).get({
       success: function (res) {
+        // console.log(art)
         // console.log(res.data)
         that.setData({
           art: res.data[0]
         })
+        // console.log(res.data[0].type)
+          if(res.data[0].type === 1) {
+            that.setData({
+              author: { name: '匿名', country: '', province: '', city: '', uid: '', image: '' }
+            })
+            
+          }
+          else{
+            db.collection('user').where({ _openid: options.uid }).get({
+              success(res) {
+                that.setData({
+                  author: { name: res.data[0].nickName, country: res.data[0].country, province: res.data[0].province, city: res.data[0].city, uid: res.data[0]._openid, image: res.data[0].avatarUrl }
+                })
+              }
+              })
+          }
+
+
+
+
+
       }
-    }),
-      db.collection('user').where({ _openid: options.uid }).get({
-        success(res) {
-          that.setData({
-            author: { name: res.data[0].nickName, country: res.data[0].country, province: res.data[0].province, city:                   res.data[0].city, uid: res.data[0]._openid, image:res.data[0].avatarUrl}
-          })
-        }
-      })
+    })
+      // console.log(that.data.art)
+      // db.collection('user').where({ _openid: options.uid }).get({
+      //   success(res) {
+          
+      //     if(art.type === 0){
+      //     that.setData({
+      //       author: { name: res.data[0].nickName, country: res.data[0].country, province: res.data[0].province, city:                   res.data[0].city, uid: res.data[0]._openid, image:res.data[0].avatarUrl}
+      //     })
+      //   }
+      //   else{
+      //       that.setData({
+      //       author: { name:'匿名', country:'', province:'', city:'', uid:'', image:''}
+      //       })
+      //   }
+      //   }
+      // })
   }
 })
