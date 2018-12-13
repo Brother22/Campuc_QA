9345a465d632a74c06f478ea9d15f4f0936a69b9
sun <sun_yyang@qq.com>
2018-12-13 22:14:17 +0800

    关注BUG

---
+++ pages/answerdetail/detail.js
@@ -0,0 +1,103 @@
+const app = getApp()
+Page({
+  data: {
+    art: {},
+    author:{},
+    comment:{}
+  },
+  onReady() {
+    wx.setNavigationBarTitle({
+      title: '详情页面'
+    })
+  },
+
+fcomment:function(){
+  var that=this
+  wx.navigateTo({
+    url: '../comment/comment?aid=' + that.data.art.aid + '&uid=' + that.data.art.uid
+  })
+},
+
+
+  fanswer:function(){
+    var that=this
+    var I = true
+    const db = wx.cloud.database()
+    if(that.data.art.type === 0){
+      console.log(2222222222222222)
+      console.log(app.globalData.id)
+    db.collection('myloveperson').where({uid:that.data.author.uid}).get({
+      success(res){
+        console.log(res.data)
+        for (var i = 0; i < res.data.length;++i){
+            if(res.data[i]._openid === app.globalData.id)
+            {
+             
+              I = false
+              break
+            }
+        }
+        if (I) {
+          db.collection('myloveperson').add({
+            data: that.data.author,
+            success(res) {
+              wx.navigateTo({
+                url: '../fsucc/fsucc'
+              })
+            }
+          })
+          }
+          else{
+          wx.navigateTo({
+            url: '../fsuccr/fsucc'
+          })
+          }
+      }
+    })
+    }
+    else {
+      wx.navigateTo({
+        url: '../fsuccr/fsucc'
+      })
+    }
+    // db.collection('myloveperson').add({
+    //   data:that.data.author
+    // })
+  },
+
+  onLoad(options) {
+    var that = this
+    const db = wx.cloud.database()
+    // console.log("123"+options.id)
+    db.collection('answer').where({ aid: options.id }).get({
+      success: function (res) {
+        // console.log(art)
+        // console.log(res.data)
+        that.setData({
+          art: res.data[0]
+        })
+        // console.log(res.data[0].type)
+          if(res.data[0].type === 1) {
+            that.setData({
+              author: { name: '匿名', country: '', province: '', city: '', uid: '', image: '' }
+            })
+            
+          }
+          else{
+            db.collection('user').where({ _openid: options.uid }).get({
+              success(res) {
+                that.setData({
+                  author: { name: res.data[0].nickName, country: res.data[0].country, province: res.data[0].province, city: res.data[0].city, uid: res.data[0]._openid, image: res.data[0].avatarUrl }
+                })
+              }
+              })
+          }
+
+
+
+
+
+      }
+    })
+  }
+})
